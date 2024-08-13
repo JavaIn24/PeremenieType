0cdcbb9a3a90f207a05025290cc5e3c22f9f5555
ярослав <yaracrut245@gmail.com>
2024-08-13 15:19:34 +0500

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main (){
+    val intAge = 24
+    val Stringname = "Ярослав"
+    val BooleanisStudent = false
+    val Doublesalary = 70000.0
+    println("Age: " +  intAge + " | " + "Name: " + Stringname + " | " + "Stedent true/false: " + BooleanisStudent + " | " +  "Salary: " + Doublesalary);
+
+}
